d676b90327bd7982ea3c325ef0eab4ccd9b822d8
System Coding <revanthnagmunagala@gmail.com>
2024-05-31 12:42:09 +0530

    v0.4 fix2

--- engine/src/EntryPoint.cpp
+++ engine/src/EntryPoint.cpp
@@ -3,9 +3,9 @@
 int main() 
 {
   Freeze::Application *application = new Freeze::Application();
-  FZ_INFO("Freeze Engine v0.3_DEV");
+  FZ_INFO("Freeze Engine v0.4_DEV");
 
-  application->OnInit(1280, 720, "Freeze Engine v0.3_DEV");
+  application->OnInit(1280, 720, "Freeze Engine v0.4_DEV");
   application->Run();
 
   delete application;
